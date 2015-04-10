706cf6652696e0562757fa835e98c1ada5bf43e6
Paul Eccleston <ecclestonp@gmail.com>
2015-04-09 23:01:34 -0500

    For some reason this doesn't work, but i have the framework for regex maching in. the functions can now be implemented

--- shell.cpp
+++ shell.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <string>
+#include <regex.h>
 
 using namespace std;
- 
+
+// Builtin regexes
+string ECHO = "\\s*echo";
+string EXIT = "\\s*exit";
+
+bool isMatch(string reg, string command)
+{
+	regex_t re;
+	int status;
+
+	if (regcomp(&re, ECHO.c_str(), REG_EXTENDED|REG_NOSUB) != 0)
+	{
+		cout << "Issue initializing regex." << endl;
+		return 1;
+	}
+	status = regexec(&re, command.c_str(), (size_t) 0, NULL, 0);
+	regfree(&re);
+
+	return !status;
+}
+
 int main(void)
 {
+
 	string prompt = "> "; // The prompt the user sees
 	bool done = false; // If the user wants to exit
 	string command; // The command that the user is inputting
@@ -14,10 +36,12 @@ int main(void)
 		cout << prompt;
 		getline(cin, command);
 
+		if (isMatch(ECHO, command))
+		{
+			cout << "That was an echo!" << endl;
+		}
 
-		done = (command.compare("exit") == 0);
+		done = isMatch(EXIT, command);
 	}
 	while (!done);
 }
-
-
