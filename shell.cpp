817124164a50d7d2c09ea68d21c369a3981640f0
Paul Eccleston <ecclestonp@gmail.com>
2015-04-10 13:18:44 -0500

    This commit includes a working echo, which is a decent example to go by for future regex work

--- shell.cpp
+++ shell.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <string>
 #include <regex.h>
+#include <stdlib.h>
+#include <cstring>
+
+// Maximum number of command line arguments to parse
+#define MAXARGS 10
 
 using namespace std;
 
 // Builtin regexes
 string ECHO = "\\s*echo";
 string EXIT = "\\s*exit";
+string ALL_ARGS = "\\s*echo\\s*(.*)$";
 
 bool isMatch(string reg, string command)
 {
@@ -24,6 +30,35 @@ bool isMatch(string reg, string command)
 	return !status;
 }
 
+int echo(string command)
+{
+	regex_t re;
+	int status;
+	size_t numGroups = re.re_nsub + 1;
+	regmatch_t *arguments = (regmatch_t *)malloc(sizeof(regmatch_t) * numGroups);
+
+	if (regcomp(&re, ALL_ARGS.c_str(), REG_EXTENDED) != 0)
+		return 1;
+
+	status = regexec(&re, command.c_str(), numGroups, arguments, 0);
+
+	if(!status) // If it was located
+	{
+		for (int x = 1; x < numGroups && arguments[x].rm_so != -1; x++)
+		{
+			size_t tempSize = (arguments[x].rm_eo - arguments[x].rm_so);
+			char *tempStr = (char *)malloc(sizeof(char *) * (tempSize + 1));
+			strncpy(tempStr, command.c_str() + arguments[x].rm_so, tempSize);
+			tempStr[tempSize] = 0;
+			cout << tempStr << endl;
+		}
+	}
+
+	// Free the newly allocated memory
+	regfree(&re);
+	free(arguments);
+}
+
 int main(void)
 {
 
@@ -38,7 +73,7 @@ int main(void)
 
 		if (isMatch(ECHO, command))
 		{
-			cout << "That was an echo!" << endl;
+			echo(command);
 		}
 
 		done = isMatch(EXIT, command);
